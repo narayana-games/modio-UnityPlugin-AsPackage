f2d575ddc84ffa37edacb678986e656eae552b04
Jashan Chittesh <jashan@narayana-games.net>
2019-09-12 11:07:23 +0200

    Added v2.1 update just as a comment

--- git-update-origin.bat
+++ git-update-origin.bat
@@ -5,3 +5,7 @@ REM From: https://help.github.com/en/articles/syncing-a-fork
 git fetch upstream
 git checkout master
 git merge upstream/master
+
+REM git fetch upstream
+REM git checkout v2.1-dev
+REM git merge upstream/v2.1-dev
